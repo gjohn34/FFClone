5a35043bfe2d5fe30c701103014e5e376a0e8f3b
gjohn34 <glenjohnson39@yahoo.com.au>
2022-04-07 13:12:38 +1000

    Add project files.

---
+++ FFClone/States/State.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FFClone
+{
+    public abstract class State
+    {
+        #region Fields
+        private Game _game;
+        private GraphicsDevice _graphicsDevice;
+        private ContentManager _content;
+        #endregion
+        public State(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
+        {
+
+        }
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+    }
+}
---
+++ FFClone/States/StateManager.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FFClone
+{
+    public sealed class StateManager
+    {
+        private static StateManager instance = null;
+        private static readonly object padlock = new object();
+        private static State _current;
+        private static State _next;
+        StateManager()
+        {
+        }
+
+        public static StateManager Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new StateManager();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void Next(State state)
+        {
+            _next = state;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (_next != null)
+            {
+                _current = _next;
+                _next = null;
+            }
+            _current.Update(gameTime);
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            _current.Draw(gameTime, spriteBatch);
+        }
+    }
+}
