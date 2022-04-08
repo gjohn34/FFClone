82c2167c3872e61c16340a63c4b2f3d5eff7f66a
gjohn34 <glenjohnson39@yahoo.com.au>
2022-04-08 14:35:18 +1000

    player walk cycle

---
+++ FFClone/Sprites/PlayerSprite.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFClone.Sprites
+{
+    public class PlayerSprite : AnimatedSprite
+    {
+        private Vector2 _velocity = Vector2.Zero;
+        private float _speed = 4;
+
+        public PlayerSprite(Texture2D texture, int rows, int columns) : base(texture, rows, columns)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+            Playing = true;
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                _velocity.Y = -_speed;
+                Facing = Facing.Up;
+            }
+            else if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                _velocity.Y = _speed;
+                Facing = Facing.Down;
+            } else if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                _velocity.X = -_speed;
+                Facing = Facing.Left;
+            }
+            else if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                _velocity.X = _speed;
+                Facing = Facing.Right;
+            } else
+            {
+                Playing = false;
+            }
+
+            // Update position
+            _position += _velocity;
+
+            // Stop the player when not pressing any key
+            _velocity = Vector2.Zero;
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ FFClone/Sprites/Sprite.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFClone.Sprites
+{
+    public class Sprite
+    {
+        protected Texture2D _texture;
+        protected Vector2 _position = Vector2.Zero;
+        public Vector2 Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+            }
+        }
+
+        public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+        public virtual void Update(GameTime gameTime) 
+        {
+        }
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(_texture,
+                _position,
+                Color.White);
+            spriteBatch.End();
+        }
+    }
+}
