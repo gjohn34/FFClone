41fd355aef0985a6b2a4f4730e3721afec027af5
gjohn34 <glenjohnson39@yahoo.com.au>
2022-04-10 13:47:31 +1000

    starting battle mode, add rectangle helper

---
+++ FFClone/Helpers/Shapes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame;
+
+namespace FFClone.Helpers.Shapes
+{
+    public static class RectangleHelper
+    {
+        public static void DrawRectangleWithFill(this SpriteBatch spriteBatch, Rectangle rectangle, int thickness, Color outline, Color fill)
+        {
+
+            // upper left to right
+            spriteBatch.DrawLine(rectangle.X, rectangle.Y, rectangle.X + rectangle.Width, rectangle.Y, outline, thickness);
+            // upeer right to lower right
+            spriteBatch.DrawLine(rectangle.X + rectangle.Width, rectangle.Y, rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height, outline, thickness);
+            // upper left to lower left
+            spriteBatch.DrawLine(rectangle.X, rectangle.Y, rectangle.X, rectangle.Y + rectangle.Height, outline, thickness);
+            // lower left to right
+            spriteBatch.DrawLine(rectangle.X - thickness, rectangle.Y + rectangle.Height, rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height, outline, thickness);
+
+            // fill
+            spriteBatch.FillRectangle(new Rectangle(rectangle.X, rectangle.Y + thickness, rectangle.Width - thickness, rectangle.Height - thickness), fill);
+        }
+    }
+}
---
+++ FFClone/States/BattleState.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame;
+using FFClone.Helpers.Shapes;
+
+namespace FFClone.States
+{
+    public class BattleState : State
+    {
+        public BattleState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            int width = _vW / 2 - (int)(_font.MeasureString("battlemode").X / 2);
+            spriteBatch.DrawString(_font, "battlemode", new Vector2(width, 0), Color.White);
+            int height = (int)(_vH * 0.3);
+            spriteBatch.DrawRectangleWithFill(new Rectangle(10, _vH - height, _vW - 10, height - 10), 10, Color.Black, Color.CadetBlue);
+            spriteBatch.End();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+    }
+}
