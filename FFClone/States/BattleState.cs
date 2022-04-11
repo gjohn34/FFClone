2e4823a066453727646daf288b23c1587c11b3ec
gjohn34 <glenjohnson39@yahoo.com.au>
2022-04-12 09:26:23 +1000

    battle state layout w/ resize

--- FFClone/States/BattleState.cs
+++ FFClone/States/BattleState.cs
@@ -1,15 +1,36 @@
-﻿using Microsoft.Xna.Framework;
+﻿using FFClone.Controls;
+using FFClone.Helpers.Shapes;
+using FFClone.Models;
+using FFClone.Sprites;
+using FFClone.Transitions;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using MonoGame;
-using FFClone.Helpers.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace FFClone.States
 {
     public class BattleState : State
     {
+        private List<Hero> _party;
+        private int _thickness = 10;
+        private Rectangle _bottomBar;
+        private Rectangle _cBar;
+        private MenuList _command;
+
         public BattleState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
         {
+            _party = GameInfo.Instance.Party;
+            int height = (int)(_vH * 0.3);
+            int menuYPos = _vH - height + _thickness;
+            int commandWidth = (int)(_vW * 0.35);
+            _bottomBar = new Rectangle(_thickness, _vH - height, _vW - _thickness, height - _thickness);
+            _cBar = new Rectangle(_thickness, menuYPos, commandWidth - _thickness, height - 2 * _thickness - 1);
+            _command = new MenuList(new List<string> { "Attack", "Defend", "Spell" }, _cBar, _font);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -19,28 +40,52 @@ namespace FFClone.States
             spriteBatch.DrawString(_font, "battlemode", new Vector2(width, 0), Color.White);
             int height = (int)(_vH * 0.3);
             int thickness = 10;
-            spriteBatch.DrawRectangleWithFill(new Rectangle(thickness, _vH - height, _vW - thickness, height - thickness), thickness, Color.Black, Color.CadetBlue);
-            
+
+            // bottom bar
+            spriteBatch.DrawRectangleWithFill(_bottomBar, _thickness, Color.Black, Color.CadetBlue);
+
             // heroes
+            {
             spriteBatch.DrawRectangleWithFill(new Rectangle((int)(_vW - (_vW * 0.20)), (int)(_vH * 0.10), 50, 50), 1, Color.Black, Color.Red);
-            spriteBatch.DrawRectangleWithFill(new Rectangle((int)(_vW - (_vW * 0.20)), (int)(_vH * 0.20) + 50, 50, 50), 1, Color.Black, Color.Yellow);
+            spriteBatch.DrawRectangleWithFill(new Rectangle((int)(_vW - (_vW * 0.20)), (int)(_vH * 0.20) + 50, 50, 50), 1, Color.Black, Color.Blue);
             spriteBatch.DrawRectangleWithFill(new Rectangle((int)(_vW - (_vW * 0.20)), (int)(_vH * 0.30) + 100, 50, 50), 1, Color.Black, Color.Green);
 
             // enemies
             spriteBatch.DrawRectangleWithFill(new Rectangle((int)(_vW * 0.20), (int)(_vH * 0.10), 50, 50), 1, Color.Black, Color.Purple);
             spriteBatch.DrawRectangleWithFill(new Rectangle((int)(_vW * 0.20), (int)(_vH * 0.20) + 50, 50, 50), 1, Color.Black, Color.Purple);
             spriteBatch.DrawRectangleWithFill(new Rectangle((int)(_vW * 0.20), (int)(_vH * 0.30) + 100, 50, 50), 1, Color.Black, Color.Purple);
+            }
 
             // commands
             int commandWidth = (int)(_vW * 0.35);
             int menuYPos = _vH - height + thickness;
-            int availableSpace = _vW - thickness - commandWidth;
-            spriteBatch.DrawRectangle(new Rectangle(thickness, menuYPos, commandWidth - thickness, height - 2 * thickness - 1), Color.Yellow);
+            int availableSpace = _vW - thickness - commandWidth + 3;
+            spriteBatch.End();
+
+            _command.Draw(gameTime, spriteBatch);
+            
+            spriteBatch.Begin();
+
+            //spriteBatch.DrawRectangle(new Rectangle(thickness, menuYPos, commandWidth - thickness, height - 2 * thickness - 1), Color.Yellow);
             int charInfoWidth = (int)(availableSpace * 0.33);
 
-            spriteBatch.DrawRectangle(new Rectangle(commandWidth + 1, _vH - height + thickness, charInfoWidth, height - 2 * thickness - 1), Color.Lime);
-            spriteBatch.DrawRectangle(new Rectangle(commandWidth + charInfoWidth + 3, _vH - height + thickness, charInfoWidth, height - 2 * thickness - 1), Color.Red);
-            spriteBatch.DrawRectangle(new Rectangle(commandWidth + 2 * charInfoWidth + 5, _vH - height + thickness, charInfoWidth, height - 2 * thickness - 1), Color.Blue);
+            // offset by 1 for border pixel
+            int xOffset = commandWidth + 1;
+            Stack<Color> colors = new Stack<Color>();
+            colors.Push(Color.Red);
+            colors.Push(Color.Blue);
+            colors.Push(Color.Green);
+
+            foreach (Hero hero in _party)
+            {
+                Vector2 v = new Vector2(xOffset, menuYPos);
+                spriteBatch.DrawRectangleWithFill(new Rectangle(xOffset, menuYPos, charInfoWidth, height - 2 * thickness - 1), 1, Color.Black, colors.Pop());
+                spriteBatch.DrawString(_font, hero.Name, v, Color.White);
+                spriteBatch.DrawString(_font, $"{hero.HP} / {hero.MaxHP}", new Vector2(xOffset, menuYPos + (int)_font.LineSpacing), Color.White);
+                // offset by 2 for box and border pixels
+                xOffset += charInfoWidth + 1;
+            }
+
 
 
 
@@ -49,6 +94,19 @@ namespace FFClone.States
 
         public override void Update(GameTime gameTime)
         {
+            _command.Update(gameTime);
+        }
+
+        public override void Resized()
+        {
+            base.Resized();
+            int height = (int)(_vH * 0.3);
+            int menuYPos = _vH - height + _thickness;
+            int commandWidth = (int)(_vW * 0.35);
+            _bottomBar = new Rectangle(_thickness, _vH - height, _vW - _thickness, height - _thickness);
+            _cBar = new Rectangle(_thickness, menuYPos, commandWidth - _thickness, height - 2 * _thickness - 1);
+            _command.Rectangle = _cBar;
+            _command.Resized();
         }
     }
 }
