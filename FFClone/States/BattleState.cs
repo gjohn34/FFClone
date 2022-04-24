ab10641b6fdf89474c851dd8ef00db03bab32e1a
gjohn34 <glenjohnson39@yahoo.com.au>
2022-04-24 12:08:26 +1000

    refactor battle bar

--- FFClone/States/BattleState.cs
+++ FFClone/States/BattleState.cs
@@ -1,5 +1,4 @@
 ﻿using FFClone.Controls;
-using FFClone.Helpers.Keyboard;
 using FFClone.Helpers.Shapes;
 using FFClone.Models;
 using FFClone.Sprites;
@@ -7,7 +6,6 @@ using FFClone.Transitions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using MonoGame;
 using System;
 using System.Collections.Generic;
@@ -15,112 +13,152 @@ using System.Diagnostics;
 
 namespace FFClone.States
 {
-    public class Triangle
+    public class BattleBars
     {
-        public Vector2 Point1 { get; set; }
-        public Vector2 Point2 { get; set; }
-        public Vector2 Point3 { get; set; }
-        public Triangle(Vector2 point, Point size)
-        {
-
-            Point1 = point;
-            Point2 = new Vector2(point.X - size.X, point.Y - size.Y);
-            Point3 = new Vector2(point.X - size.X, point.Y + size.Y);
-        }
-        public Triangle(Vector2 p1, Vector2 p2, Vector2 p3)
-        {
-            Point1 = p1;
-            Point2 = p2;
-            Point3 = p3;
-        }
-        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-        {
-            Primitives2D.DrawLine(spriteBatch, Point1, Point2, Color.Black);
-            Primitives2D.DrawLine(spriteBatch, Point1, Point3, Color.Black);
-            Primitives2D.DrawLine(spriteBatch, Point2, Point3, Color.Black);
-        }
-        public void Update(Vector2 point, Point size)
-        {
-            Point1 = point;
-            Point2 = new Vector2(point.X - size.X, point.Y - size.Y);
-            Point3 = new Vector2(point.X - size.X, point.Y + size.Y);
-
-        }
-    }
-    public class Prompt : IComponent
-    {
-        private KeyboardState _previous;
-        public List<Vector2> Vectors;
-        private int _promptOn = 0;
-        private Triangle _selector;
+        private Game1 _game;
+        private GraphicsDevice _graphicsDevice;
+        private ContentManager _content;
+        private List<Hero> _party;
+        private List<Enemy> _enemies;
+        private SpriteFont _font;
+        private int _thickness;
+        private int _vH;
+        private int _vW;
+        private Stack<IComponent> _menuStack = new Stack<IComponent>();
+        private Rectangle _cBar;
         private Battle _battle;
-        private Ability _ability;
-        public Rectangle Rectangle { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public Prompt(List<Vector2> vectors, Battle battle, Ability ability)
+        public Rectangle Rectangle;
+        public BattleBars(Game1 game, GraphicsDevice graphicsDevice, ContentManager content, List<Hero> party, List<Enemy> enemies, Battle battle, Rectangle rectangle)
         {
-            _previous = Keyboard.GetState();
-            Vectors = vectors;
-            _selector = new Triangle(vectors[_promptOn], new Point(35, 12));
+            _game = game;
+            _graphicsDevice = graphicsDevice;
+            _content = content;
+            _party = party;
+            _enemies = enemies;
             _battle = battle;
-            _ability = ability;
+            _font = content.Load<SpriteFont>("Font/font");
+            _vH = game.Window.ClientBounds.Height;
+            _vW = game.Window.ClientBounds.Width;
+            Rectangle = rectangle;
+            _thickness = rectangle.X;
+
+            // TODO - This refactor this w/ resize
+            int height = (int)(_vH * 0.3);
+            int menuYPos = _vH - height + _thickness;
+            int commandWidth = (int)(_vW * 0.35);
+
+            _cBar = new Rectangle(rectangle.X, menuYPos, commandWidth - rectangle.X, rectangle.Height - rectangle.X - 1);
+            MenuList commandMenu = new MenuList(new List<string> { "Attack", "Defend", "Spell" }, _cBar, _font);
+            foreach (MenuItem item in commandMenu.MenuItems)
+            {
+                item.Touch += HandleHandlers(item.Text);
+            }
+            _menuStack.Push(commandMenu);
         }
 
         public void Update(GameTime gameTime)
         {
-            KeyboardState keyboard = Keyboard.GetState();
-            bool changed = false;
 
-            if (keyboard.Released(_previous, Keys.Enter))
+            if (_menuStack.TryPeek(out _))
             {
-                _battle.SetSelected(_promptOn, _ability);
-            } else if (keyboard.Released(_previous, Keys.Escape))
-            {
-                _battle.DisablePrompt();
-            } else if (keyboard.Released(_previous, Keys.Down))
-            {
-                changed = true;
-                _promptOn += 1;
-                if (_promptOn >= Vectors.Count)
-                {
-                    _promptOn = 0;
-                }
+                _menuStack.Peek().Update(gameTime);
             }
-            else if (keyboard.Released(_previous, Keys.Up))
-            {
-                changed = true;
-                _promptOn -= 1;
-                if (_promptOn < 0)
-                {
-                    _promptOn = Vectors.Count - 1;
-                }
-            }
-            if (changed)
-            {
-                _selector.Update(Vectors[_promptOn], new Point(35, 12));
-            }
-
-            _previous = keyboard;
         }
+
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            _selector.Draw(gameTime, spriteBatch);
+
+            // bottom bar
+            spriteBatch.DrawRectangleWithFill(Rectangle, _thickness, Color.Black, Color.CadetBlue);
+
+            // commands
+            int commandWidth = (int)(_vW * 0.35);
+            int menuYPos = _vH - (int)(_vH * 0.3) + Rectangle.X;
+            int availableSpace = _vW - Rectangle.X - commandWidth + 3;
+
+            if (_menuStack.TryPeek(out _))
+            {
+                foreach (IComponent item in _menuStack)
+                {
+                    item.Draw(gameTime, spriteBatch);
+                }
+            }
+            int charInfoWidth = (int)(availableSpace * 0.33);
+
+            // offset by 1 for border pixel
+            int xOffset = commandWidth + 1;
+            double yOffset = 0.1;
+
+
+            foreach (Hero hero in _party)
+            {
+                Vector2 v = new Vector2(xOffset, menuYPos);
+                yOffset += 0.2;
+                
+                // command box
+                spriteBatch.DrawRectangleWithFill(new Rectangle(xOffset, menuYPos, charInfoWidth, Rectangle.Height - Rectangle.X - 1), 1, Color.Black, hero.Color);
+                spriteBatch.DrawString(_font, hero.Name, v, Color.White);
+                spriteBatch.DrawString(_font, $"{hero.HP} / {hero.MaxHP}", new Vector2(xOffset, menuYPos + (int)_font.LineSpacing), Color.White);
+
+                // offset by 2 for box and border pixels
+                xOffset += charInfoWidth + 1;
+            }
         }
-        public void Resized()
+
+        internal void Resized()
         {
-            _selector = new Triangle(Vectors[_promptOn], new Point(35, 12));
-            //throw new NotImplementedException();
+            _vH = _game.Window.ClientBounds.Height;
+            _vW = _game.Window.ClientBounds.Width;
+            int height = (int)(_vH * 0.3);
+            int menuYPos = _vH - height + _thickness;
+            Rectangle = new Rectangle(_thickness, _vH - height, _vW - _thickness, height - _thickness);
+            _cBar = new Rectangle(Rectangle.X, menuYPos, (int)(_vW * 0.35) - Rectangle.X, height - Rectangle.X - 1);
+            IComponent[] x = _menuStack.ToArray();
+            x[0].Rectangle = _cBar;
+            foreach (IComponent component in _menuStack)
+            {
+                component.Resized();
+            }
+        }
+
+        public EventHandler HandleHandlers(string option)
+        {
+            return option switch
+            {
+                "Attack" => (object a, EventArgs e) =>
+                {
+                    List<Vector2> vectors = new List<Vector2>();
+                    foreach (Enemy enemy in _enemies)
+                    {
+                        vectors.Add(new Vector2(enemy.BattleSprite.Position.X, enemy.BattleSprite.Position.Y + (int)(0.5 * enemy.BattleSprite.Height)));
+                        //vectors.Add(new Vector2((int)(_vW * 0.20) - 25, (int)(_vH * yOffset) + 25));
+                        //yOffset += 0.2;
+                    }
+                    _battle.EnablePrompt(vectors, new Ability("Attack"));
+                    // hardcoded 25's to center. need to fix that
+                    //_menuStack.Push();
+                }
+                ,
+                "Defend" => (object a, EventArgs e) =>
+                {
+                    _battle.Defend();
+                }
+                ,
+                "Spell" => (object a, EventArgs e) => Debug.WriteLine("Spell"),
+                _ => (a, e) => { }
+                ,
+            };
         }
     }
-
     public class BattleState : State
     {
         private List<Hero> _party;
         private List<Enemy> _enemies;
         private Battle _battle;
+        private BattleBars _battleBar;
+
+        // TODO make thickness reactive
         private int _thickness = 10;
-        private Rectangle _bottomBar;
-        private Rectangle _cBar;
-        private Stack<IComponent> _menuStack = new Stack<IComponent>();
 
 
         public BattleState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
@@ -146,96 +184,22 @@ namespace FFClone.States
                 },
             };
             _enemies = new List<Enemy> { 
-                new Enemy("Purple Guy", Color.Purple, statBlock) };
-            _battle = new Battle(_party, _enemies, game, graphicsDevice, content);
-            // TODO - This refactor this w/ resize
-            int height = (int)(_vH * 0.3);
-            int menuYPos = _vH - height + _thickness;
-            int commandWidth = (int)(_vW * 0.35);
-            _bottomBar = new Rectangle(_thickness, _vH - height, _vW - _thickness, height - _thickness);
-            _cBar = new Rectangle(_thickness, menuYPos, commandWidth - _thickness, height - 2 * _thickness - 1);
-            MenuList commandMenu = new MenuList(new List<string> { "Attack", "Defend", "Spell" }, _cBar, _font);
-            foreach (MenuItem item in commandMenu.MenuItems)
-            {
-                item.Touch += HandleHandlers(item.Text);
+                new Enemy("Purple Guy", Color.Purple, statBlock) 
             }
-            _menuStack.Push(commandMenu);
+            ;
+            _battle = new Battle(_party, _enemies, game, graphicsDevice, content);
+            
+            Rectangle rectangle = new Rectangle(_thickness, _vH - (int)(_vH * 0.3), _vW - _thickness, (int)(_vH * 0.3) - _thickness);
+            _battleBar = new BattleBars(game, graphicsDevice, content, _party, _enemies, _battle, rectangle);
         }
 
-        public EventHandler HandleHandlers(string option)
-        {
-            return option switch
-            {
-                "Attack" => (object a, EventArgs e) => 
-                {
-                    List<Vector2> vectors = new List<Vector2>();
-                    foreach (Enemy enemy in _enemies)
-                    {
-                        vectors.Add(new Vector2(enemy.BattleSprite.Position.X, enemy.BattleSprite.Position.Y + (int)(0.5 * enemy.BattleSprite.Height)));
-                        //vectors.Add(new Vector2((int)(_vW * 0.20) - 25, (int)(_vH * yOffset) + 25));
-                        //yOffset += 0.2;
-                    }
-                    _battle.EnablePrompt(vectors, new Ability("Attack"));
-                    // hardcoded 25's to center. need to fix that
-                    //_menuStack.Push();
-                }
-                ,
-                "Defend" => (object a, EventArgs e) => 
-                {
-                    _battle.Defend();
-                },
-                "Spell" => (object a, EventArgs e) => Debug.WriteLine("Spell"),
-                _ => (a, e) => { },
-            };
-        }
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
             int width = _vW / 2 - (int)(_font.MeasureString("battlemode").X / 2);
             spriteBatch.DrawString(_font, "battlemode", new Vector2(width, 0), Color.White);
-            int height = (int)(_vH * 0.3);
-            int thickness = 10;
-
-            // bottom bar
-            spriteBatch.DrawRectangleWithFill(_bottomBar, _thickness, Color.Black, Color.CadetBlue);
-
-            // commands
-            int commandWidth = (int)(_vW * 0.35);
-            int menuYPos = _vH - height + thickness;
-            int availableSpace = _vW - thickness - commandWidth + 3;
-
-            //_command.Draw(gameTime, spriteBatch);
-            if (_menuStack.TryPeek(out _))
-            {
-                foreach (IComponent item in _menuStack)
-                {
-                    item.Draw(gameTime, spriteBatch);
-                }
-            }
-            //spriteBatch.DrawRectangle(new Rectangle(thickness, menuYPos, commandWidth - thickness, height - 2 * thickness - 1), Color.Yellow);
-            int charInfoWidth = (int)(availableSpace * 0.33);
-
-            // offset by 1 for border pixel
-            int xOffset = commandWidth + 1;
-            double yOffset = 0.1;
-
             _battle.Draw(gameTime, spriteBatch);
-
-            foreach (Hero hero in _party)
-            {
-                Vector2 v = new Vector2(xOffset, menuYPos);
-                // player sprite
-                //spriteBatch.DrawRectangleWithFill(new Rectangle((int)(_vW - (_vW * 0.20)), (int)(_vH * yOffset), 50, 50), 1, Color.Black, hero.Color);
-                yOffset += 0.2;
-                // command box
-                spriteBatch.DrawRectangleWithFill(new Rectangle(xOffset, menuYPos, charInfoWidth, height - 2 * thickness - 1), 1, Color.Black, hero.Color);
-                spriteBatch.DrawString(_font, hero.Name, v, Color.White);
-                spriteBatch.DrawString(_font, $"{hero.HP} / {hero.MaxHP}", new Vector2(xOffset, menuYPos + (int)_font.LineSpacing), Color.White);
-
-                // offset by 2 for box and border pixels
-                xOffset += charInfoWidth + 1;
-            }
-
+            _battleBar.Draw(gameTime, spriteBatch);
             spriteBatch.End();
         }
 
@@ -269,10 +233,7 @@ namespace FFClone.States
                     }
                     else
                     {
-                        if (_menuStack.TryPeek(out _))
-                        {
-                            _menuStack.Peek().Update(gameTime);
-                        }
+                        _battleBar.Update(gameTime);
                     }
                     break;
                 case BattleScene.AnimatingStart:
@@ -290,17 +251,7 @@ namespace FFClone.States
         public override void Resized()
         {
             base.Resized();
-            int height = (int)(_vH * 0.3);
-            int menuYPos = _vH - height + _thickness;
-            int commandWidth = (int)(_vW * 0.35);
-            _bottomBar = new Rectangle(_thickness, _vH - height, _vW - _thickness, height - _thickness);
-            _cBar = new Rectangle(_thickness, menuYPos, commandWidth - _thickness, height - 2 * _thickness - 1);
-            IComponent[] x = _menuStack.ToArray();
-            x[0].Rectangle = _cBar;
-            foreach (IComponent component in _menuStack)
-            {
-                component.Resized();
-            }
+            _battleBar.Resized();
             _battle.Resized();
             //_command.Rectangle = _cBar;
             //_command.Resized();
