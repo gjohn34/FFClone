519137137cf3cb73c9ea936e09e0b567352dcf1d
gjohn34 <glenjohnson39@yahoo.com.au>
2022-05-01 09:13:46 +1000

    responsive horizontal menu list

--- FFClone/Controls/MenuList.cs
+++ FFClone/Controls/MenuList.cs
@@ -56,32 +56,57 @@ namespace FFClone.States
 
     }
 
+
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+
     public class MenuList : IComponent
     {
-        private enum Orientation
-        {
-            Horizontal,
-            Vertical
-        }
         private KeyboardState _previousState;
-        private Orientation _orientation = Orientation.Vertical;
-        private int _columns = 1;
+        private Orientation _orientation;
+        private int _columns;
         public Rectangle Rectangle { get; set; }
+
+        private int _minCellWidth;
+        private int _minCellHeight;
+        private int _rows;
+
         public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
         private int Selected { get; set; } = 0;
-        public MenuList(List<string> menuItems, Rectangle rectangle, SpriteFont spriteFont, Func<string, EventHandler> onSubmit)
+        public MenuList(List<string> menuItems, Rectangle rectangle, SpriteFont spriteFont, Func<string, EventHandler> onSubmit, Orientation orientation = Orientation.Vertical, int columns = 1)
         {
+            _orientation = orientation;
+            _columns = columns;
             Rectangle = rectangle;
-            foreach (string option in menuItems)
+            _minCellWidth = 0;
+            _minCellHeight = 0;
+            _rows = (int)Math.Ceiling((decimal)menuItems.Count / columns);
+            if (_orientation == Orientation.Horizontal)
             {
-                MenuItems.Add(new MenuItem(option, GeneratePosition(menuItems.IndexOf(option), menuItems.Count), spriteFont, Color.Gray, onSubmit(option)));
+                foreach (string option in menuItems)
+                {
+                    int fontWidth = (int)spriteFont.MeasureString(option).X;
+                    int fontHeight = (int)spriteFont.MeasureString(option).Y;
+                    if (fontWidth > _minCellWidth)
+                        _minCellWidth = fontWidth;
+                    if (fontHeight > _minCellHeight)
+                        _minCellHeight = fontHeight;
+                }
+
+            }
+            for (int i = 0; i < menuItems.Count; i++)
+            {
+                MenuItems.Add(new MenuItem(menuItems[i], GeneratePosition(i, menuItems.Count), spriteFont, Color.Gray, onSubmit(menuItems[i])));
             }
             MenuItems[Selected].Selected = true;
 
         }
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.DrawRectangleWithFill(Rectangle, 0, Color.Black, Color.White);
+            spriteBatch.DrawRectangleWithFill(Rectangle, 0, Color.Black, _orientation == Orientation.Vertical ? Color.White : Color.Black);
             foreach (MenuItem menuItem in MenuItems)
                 menuItem.Draw(gameTime, spriteBatch);
 
@@ -99,24 +124,34 @@ namespace FFClone.States
         }
         private Rectangle GeneratePosition(int index, int length)
         {
-            int spaceBetweenY, spaceBetweenX, cellHeight, initialYPos, initialXPos, pushDown, pushDown2;
+            int spaceBetweenY, spaceBetweenX, cellHeight, cellWidth, initialYPos, initialXPos, pushDown, pushDown2;
             double cellPositionPercentage;
             switch (_orientation)
             {
                 case Orientation.Horizontal:
-                    // 100
-                    spaceBetweenY = Rectangle.Height / (_columns / length);
-                    // 33
-                    spaceBetweenX = Rectangle.Width / _columns;
-                    // 75
-                    cellHeight = (int)Math.Ceiling((0.75 * spaceBetweenY));
 
-                    initialYPos = (int)Math.Floor((double)(index / _columns)) * spaceBetweenY + Rectangle.Y;
-                    initialXPos = (int)Math.Floor((double)(index / _columns)) *spaceBetweenX + Rectangle.X;
-                    cellPositionPercentage = (double)index / (double)(length - 1);
-                    return new Rectangle(Rectangle.X, initialYPos, Rectangle.Width, cellHeight);
+                    int availableWidth = Rectangle.Width - (_columns * _minCellWidth);
+                    spaceBetweenX = availableWidth / (_columns);
 
-                    break;
+                    int availableHeight = Rectangle.Height;
+                    int totalHeight = _minCellHeight * _rows;
+                    //if (totalHeight > availableHeight)
+                    //{
+                    //    // help me jesus
+                    //} else
+                    //{
+                    availableHeight -= totalHeight;
+                    //}
+
+                    spaceBetweenY = availableHeight / (_rows);
+
+                    initialYPos = (int)Math.Floor((double)(index / _columns)) * _minCellHeight + Rectangle.Y + ((int)Math.Floor((double)(index / _columns)) * spaceBetweenY) + (int)(0.5 * spaceBetweenY);
+                    initialXPos = ((index % _columns) * _minCellWidth) + Rectangle.X + ((index % _columns) * spaceBetweenX + (int)(0.5 * spaceBetweenX));
+                    //cellPositionPercentage = (double)index / (double)(length - 1);
+
+
+                    Rectangle rect = new Rectangle(initialXPos, initialYPos, _minCellWidth, _minCellHeight);
+                    return rect;
                 case Orientation.Vertical:
                     spaceBetweenY = Rectangle.Height / length;
                     cellHeight = (int)Math.Ceiling((0.75 * spaceBetweenY));
@@ -146,14 +181,32 @@ namespace FFClone.States
             if (state.IsKeyDown(Keys.Up) && _previousState.IsKeyUp(Keys.Up))
             {
                 MenuItems[Selected].Selected = false;
-                Selected -= 1;
+                Selected -= (_orientation == Orientation.Vertical ? 1 : _columns);
                 changed = true;
             }
             else if (state.IsKeyDown(Keys.Down) && _previousState.IsKeyUp(Keys.Down))
             {
                 MenuItems[Selected].Selected = false;
-                Selected += 1;
+                Selected += (_orientation == Orientation.Vertical ? 1 : _columns);
                 changed = true;
+            }
+            else if (state.IsKeyDown(Keys.Left) && _previousState.IsKeyUp(Keys.Left))
+            {
+                if (_orientation == Orientation.Horizontal)
+                {
+                    MenuItems[Selected].Selected = false;
+                    Selected -= 1;
+                    changed = true;
+                }
+            }
+            else if (state.IsKeyDown(Keys.Right) && _previousState.IsKeyUp(Keys.Right))
+            {
+                if (_orientation == Orientation.Horizontal)
+                {
+                    MenuItems[Selected].Selected = false;
+                    Selected += 1;
+                    changed = true;
+                }
             }
             else if (state.IsKeyUp(Keys.Enter) && _previousState.IsKeyDown(Keys.Enter))
 
